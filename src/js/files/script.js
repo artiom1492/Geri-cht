31e5ee2b202ee1902150716abaa7d72c07b1cca6
Artem <artiom1492@gmail.com>
2022-08-22 05:38:03 +0300

    1.1

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,9 @@
 import { isMobile } from "./functions.js";
 // Подключение списка активных модулей
 import { flsModules } from "./modules.js";
+
+
+window.addEventListener("load", function(e) {
+    document.body.insertAdjacentHTML('beforeend', `<div class="main-pg"></div>`)
+
+});
